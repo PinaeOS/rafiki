e2e610ee115ad165a67b5fd2dc89a3fc87f82bef
Yugeng Hui <interhuiyg@163.com>
2015-10-15 00:08:55 +0800

    Update SimpleTrigger's constructor and comments

--- src/main/java/org/pinae/rafiki/trigger/AbstractTrigger.java
+++ src/main/java/org/pinae/rafiki/trigger/AbstractTrigger.java
@@ -10,15 +10,44 @@ import java.util.Date;
  */
 public abstract class AbstractTrigger implements Trigger {
 
+	/*
+	 * 触发器名称
+	 */
 	private String name;
 
-	private Date endTime = null; 
+	/*
+	 * 触发器开始时间
+	 */
 	private Date startTime = new Date();
+	
+	/*
+	 * 触发器结束时间
+	 */
+	private Date endTime = null; 
+	
+	/*
+	 * 是否循环执行
+	 */
 	private boolean repeat = true;
+	
+	/*
+	 * 触发器循环周期 (ms)
+	 */
 	private long repeatInterval = 1000;
+	
+	/*
+	 * 触发器循环次数
+	 */
 	private int repeatCount = 0;
+	
+	/*
+	 * 触发器开始延迟(ms)
+	 */
 	private long startDelayTime = 0;
 
+	/*
+	 * 触发器命中次数
+	 */
 	private long executeCount = 0;
 	
 	/**
@@ -26,6 +55,15 @@ public abstract class AbstractTrigger implements Trigger {
 	 */
 	public AbstractTrigger() {
 		name = this.toString();
+	}
+	
+	/**
+	 * 构造函数
+	 * 
+	 * @param name 触发器名称
+	 */
+	public AbstractTrigger(String name) {
+		this.name = name;
 	}
 
 	public String getName() {
--- src/main/java/org/pinae/rafiki/trigger/impl/SimpleTrigger.java
+++ src/main/java/org/pinae/rafiki/trigger/impl/SimpleTrigger.java
@@ -12,6 +12,24 @@ public class SimpleTrigger extends AbstractTrigger {
 	
 	private long lastExecuteTime = 0;
 	
+	/**
+	 * 构造函数
+	 */
+	public SimpleTrigger() {
+		
+	}
+	
+	/**
+	 * 构造函数
+	 * 
+	 * @param repeatCount 循环次数
+	 * @param repeatInterval 循环周期
+	 */
+	public SimpleTrigger(int repeatCount, long repeatInterval) {
+		super.setRepeatCount(repeatCount);
+		super.setRepeatInterval(repeatInterval);
+	}
+	
 	@Override
 	public boolean match() {
 		
