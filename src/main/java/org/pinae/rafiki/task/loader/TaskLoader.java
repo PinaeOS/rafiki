d47fa042cbc134d462b58ed0b551d131368655ad
Yugeng Hui <interhuiyg@163.com>
2018-05-24 23:52:49 +0800

    Add TaskLoader and its implements class, XmlTaskLoader/JsonTaskLoader/YamlTaskLoader

---
+++ src/main/java/org/pinae/rafiki/task/loader/TaskLoader.java
@@ -0,0 +1,9 @@
+package org.pinae.rafiki.task.loader;
+
+import org.pinae.rafiki.task.Task;
+
+public abstract class TaskLoader {
+	
+	public abstract Task load();
+	
+}
---
+++ src/main/java/org/pinae/rafiki/task/loader/YamlTaskLoader.java
@@ -0,0 +1,12 @@
+package org.pinae.rafiki.task.loader;
+
+import org.pinae.rafiki.task.Task;
+
+public class YamlTaskLoader extends TaskLoader {
+
+	@Override
+	public Task load() {
+		return null;
+	}
+
+}
